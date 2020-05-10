3140d0dd99cef0ddcb3027cfa8f848c79c95f016
sametz <sametz@udel.edu>
2020-05-09 23:54:19 -0400

    converted to using nmrsim. Note nmrsim span plus sparse deprecation warnings: nmrsim needs update. Updated requirements.txt. Added a test binder button to README.rst

--- plots.py
+++ plots.py
@@ -1,13 +1,16 @@
-from ipywidgets import interactive
-import matplotlib as mpl
-import matplotlib.pyplot as plt
+# from ipywidgets import interactive
+# import matplotlib as mpl
+# import matplotlib.pyplot as plt
 import numpy as np
-import plotly
+# import plotly
 import plotly.graph_objs as go
-from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+from plotly.offline import iplot  #, download_plotlyjs, init_notebook_mode, plot
 
-from nmrtools.nmrmath import first_order, nspinspec
-from nmrtools.nmrplot import add_signals
+# from nmrtools.nmrmath import first_order, nspinspec
+# from nmrtools.nmrplot import add_signals
+from nmrsim.firstorder import multiplet
+from nmrsim.math import add_lorentzians
+from nmrsim.qm import qm_spinsystem
 
 layout = go.Layout(
     xaxis={'autorange': 'reversed'}
@@ -15,12 +18,12 @@ layout = go.Layout(
 
 
 def interactive_dd(J1=3.0, J2=7.0, w=0.5):
-    args = (J1, J2)
+    # args = (J1, J2)
     x = np.linspace(85, 115, 8000)
     signal = (100.0, 1)
     couplings = [(J1, 1), (J2, 1)]
-    spectrum = first_order(signal, couplings)
-    y = add_signals(x, spectrum, w)
+    spectrum = multiplet(signal, couplings)
+    y = add_lorentzians(x, spectrum, w)
     obj = go.Scatter(x=x, y=y)
     data = [obj]
     fig = go.Figure(data=data, layout=layout)
@@ -28,12 +31,12 @@ def interactive_dd(J1=3.0, J2=7.0, w=0.5):
 
 
 def interactive_ddd(J1=4.0, J2=10.0, J3=12.0):
-    args = (J1, J2, J3)
+    # args = (J1, J2, J3)
     x = np.linspace(75, 125, 8000)
     signal = (100.0, 1)
     couplings = [(J1, 1), (J2, 1), (J3, 1)]
-    spectrum = first_order(signal, couplings)
-    y = add_signals(x, spectrum, 0.5)
+    spectrum = multiplet(signal, couplings)
+    y = add_lorentzians(x, spectrum, 0.5)
     obj = go.Scatter(x=x, y=y)
     data = [obj]
     fig = go.Figure(data=data, layout=layout)
@@ -45,8 +48,8 @@ def interactive_dddd(J1=6.0, J2=6.0, J3=10.0, J4=16.0):
     x = np.linspace(75, 125, 8000)
     signal = (100.0, 1)
     couplings = [(J1, 1), (J2, 1), (J3, 1), (J4, 1)]
-    spectrum = first_order(signal, couplings)
-    y = add_signals(x, spectrum, 0.5)
+    spectrum = multiplet(signal, couplings)
+    y = add_lorentzians(x, spectrum, 0.5)
     obj = go.Scatter(x=x, y=y)
     data = [obj]
     fig = go.Figure(data=data, layout=layout)
@@ -59,8 +62,8 @@ def interactive_AB(v1=150.0, v2=50.0, J=10.0):
     Js = np.array(
         [[0.0, J],
          [J, 0.0]])
-    spectrum = nspinspec(freqs, Js)
-    y = add_signals(x, spectrum, 0.5)
+    spectrum = qm_spinsystem(freqs, Js)
+    y = add_lorentzians(x, spectrum, 0.5)
     obj = go.Scatter(x=x, y=y)
     data = [obj]
     fig = go.Figure(data=data, layout=layout)
@@ -74,8 +77,8 @@ def interactive_ABX(va=110.0, vb=90.0, vx=200.0, Jax=5.0, Jbx=10.0, Jab=13.0):
         [[0.0, Jab, Jax],
          [Jab, 0.0, Jbx],
          [Jax, Jbx, 0]])
-    spectrum = nspinspec(freqs, Js)
-    y = add_signals(x, spectrum, 0.5)
+    spectrum = qm_spinsystem(freqs, Js)
+    y = add_lorentzians(x, spectrum, 0.5)
     obj = go.Scatter(x=x, y=y)
     data = [obj]
     fig = go.Figure(data=data, layout=layout)
@@ -92,8 +95,8 @@ def interactive_AAXX(va=110.0, vx=90.0,
          [Jaa, 0.0, Jax_prime, Jax],
          [Jax, Jax_prime, 0, Jxx],
          [Jax_prime, Jax, Jxx, 0]])
-    spectrum = nspinspec(freqs, Js)
-    y = add_signals(x, spectrum, 0.5)
+    spectrum = qm_spinsystem(freqs, Js)
+    y = add_lorentzians(x, spectrum, 0.5)
     obj = go.Scatter(x=x, y=y)
     data = [obj]
     fig = go.Figure(data=data, layout=layout)
